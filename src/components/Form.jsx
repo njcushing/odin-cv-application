f605c32aebbbbc08e983584bbdd1758a63e22302
njcushing <129661299+njcushing@users.noreply.github.com>
2023-08-06 20:33:50 +0100

    Get first and last name inputs working with state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,73 +3,119 @@ import './../styles/Form.css'
 
 import InputSingleLine from './InputSingleLine.jsx'
 
-function Form() {
-    let page = "Home";
-    let counter = 0;
-    const CVInformation = {
+let page = "Home";
+let counter = 0;
+const CVInformation = {
+    firstName: "",
+    secondName: "",
+    emailAddress: "",
+    phoneNumber: "",
+    previousEducation: [],
+    previousEmployment: [],
+    hobbiesInterests: "",
+    references: [],
+}
+const previousEducation = () => {
+    return {
+        id: counter++,
+        institution: "",
+        qualifications: [],
+        startDate: null,
+        endDate: null,
+    }
+}
+const qualification = () => {
+    return {
+        id: counter++,
+        name: "",
+        grade: "",
+    }
+}
+const previousEmployment = () => {
+    return {
+        id: counter++,
+        company: "",
+        role: "",
+        responsibilities: "",
+        startDate: null,
+        endDate: null,
+    }
+}
+const references = () => {
+    return {
+        id: counter++,
         firstName: "",
-        secondName: "",
+        lastName: "",
+        relation: "",
         emailAddress: "",
         phoneNumber: "",
-        previousEducation: [],
-        previousEmployment: [],
-        hobbiesInterests: "",
-        references: [],
     }
-    const previousEducation = () => {
-        return {
-            id: counter++,
-            institution: "",
-            qualifications: [],
-            startDate: null,
-            endDate: null,
-        }
-    }
-    const qualification = () => {
-        return {
-            id: counter++,
-            name: "",
-            grade: "",
-        }
-    }
-    const previousEmployment = () => {
-        return {
-            id: counter++,
-            company: "",
-            role: "",
-            responsibilities: "",
-            startDate: null,
-            endDate: null,
-        }
-    }
-    const references = () => {
-        return {
-            id: counter++,
-            firstName: "",
-            lastName: "",
-            relation: "",
-            emailAddress: "",
-            phoneNumber: "",
-        }
-    }
+}
+
+function Form() {
+    const [personalInformation, setPersonalInformation] = useState({
+        firstName: "",
+        lastName: "",
+        emailAddress: "",
+        phoneNumber: "",
+    });
 
     switch (page) {
         case "Home":
-            break;
+            return (
+                <>
+                <h2>Personal Information</h2>
+                <InputSingleLine
+                    labelText="First Name(s): "
+                    inputValue={personalInformation.firstName}
+                    changeHandler={(e) => setPersonalInformation(
+                        { ...personalInformation, firstName: e.target.value }
+                    )}
+                />
+                <InputSingleLine
+                    labelText="Last Name: "
+                    inputValue={personalInformation.lastName}
+                    changeHandler={(e) => setPersonalInformation(
+                        { ...personalInformation, lastName: e.target.value }
+                    )}
+                />
+                </>
+            )
         case "PersonalInfo":
-            break;
+            return (
+                <>
+                </>
+            )
         case "Education":
-            break;
+            return (
+                <>
+                </>
+            )
         case "Employment":
-            break;
+            return (
+                <>
+                </>
+            )
         case "HobbiesInterests":
-            break;
+            return (
+                <>
+                </>
+            )
         case "References":
-            break;
+            return (
+                <>
+                </>
+            )
         case "Review":
-            break;
+            return (
+                <>
+                </>
+            )
         case "Submitted":
-            break;
+            return (
+                <>
+                </>
+            )
         default:
             return null;
     }
--- src/components/InputSingleLine.jsx
+++ src/components/InputSingleLine.jsx
@@ -1,5 +1,21 @@
-const InputSingleLine = () => {
+const InputSingleLine = ({
 
+    labelText = "Label",
+    inputValue = "",
+    changeHandler=null,
+
+}) => {
+    
+    return (
+        <>
+        <label>
+            {labelText}
+            <input type='text' value={inputValue} onChange={changeHandler}>
+            </input>
+        </label>
+        </>
+    )
+    
 }
 
 export default InputSingleLine;
